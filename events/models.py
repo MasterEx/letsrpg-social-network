fa6ea8038e25ba911d35d6c428dbf8ba7283d424
Periklis Ntanasis <pntanasis@gmail.com>
2011-06-02 20:21:39 +0300

    events models added to database

--- events/models.py
+++ events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Event(models.Model):
+	# id is auto created
+	game_master = models.CharField(max_length=10)
+	user_role = models.CharField(max_length=10)
+	date = models.DateTimeField(auto_now_add='true')
+	slots = models.PositiveSmallIntegerField()
+	slots_taken = models.IntegerField()
+	location = models.CharField(max_length=20)
+
+	def __unicode__(self):
+			return self.name
+
+class EventPlayer(models.Model):
+	userid = models.ForeignKey(User)
+	eventid = models.ForeignKey(Event)
+	type = (
+		( 'P' , 'Player'),
+		( 'M' , 'Game Master - Story Tailer'),
+	)
+
+	def __unicode__(self):
+			return self.name
