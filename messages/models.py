1f0cdcaae5f3a548e1f0399cb84228ab100414ec
Periklis Ntanasis <pntanasis@gmail.com>
2011-06-03 02:33:32 +0300

    messages application - models and syncdb

---
+++ messages/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from accounts.models import User
+
+class Messages(models.Model):
+	# id is auto generated
+	userid_sender = models.ForeignKey(User,related_name='sender')
+	userid_receiver = models.ForeignKey(User,related_name='receiver')
+	message = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add='true')
+	STATUS_CHOICES = (
+		( 'A' , 'public message'),
+		( 'P' , 'private message'),
+	)
+	status = models.CharField(max_length=1,choices=STATUS_CHOICES)
+	READ_CHOICES = (
+		( 'U' , 'unread' ),
+		( 'R' , 'read' ),
+	)
+	read = models.CharField(max_length=1,default='U',choices=READ_CHOICES)
+
+	def __unicode__(self):
+		return self.name
+
--- settings.py
+++ settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'accounts',
     'events',
     'follows',
+    'messages',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
