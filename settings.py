91c00dc3c7d09cecf46bbf027a5b1ae388266c67
Periklis Ntanasis <pntanasis@gmail.com>
2011-06-03 03:38:47 +0300

    adding admin module to INSTALLED_APPS

--- settings.py
+++ settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     'useractions',
     'ads',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
