ed6c758a057e3c75e70d5f514d228c82a28724a5
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-06-06 09:26:37 +0300

    comic list and files

---
+++ static/scripts/comic.js
@@ -0,0 +1,16 @@
+var comics;
+$(function() {
+  $.get('/static/files/comic.list', function(data) {
+    comics = data.split("\n"); clickComic();
+  });
+});
+function clickComic() {
+  $("#comic").unbind().fadeOut('fast', function() {
+    $("#comic").attr({src: '/static/images/load.gif', title: 'loading'}).fadeIn();
+    var comic = comics[Math.floor(Math.random()*comics.length)];
+    $(document.createElement("img")).attr('src', comic).load(function() {
+        alert(comic);
+      $("#comic").hide().attr({src: comic, title: 'Giantitp comics'}).fadeIn().click(clickComic);
+    });
+  });
+}
